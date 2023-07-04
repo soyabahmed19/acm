9d0f91d2aa0c3fe7e67812f190278461cec6bbb7
soyabahmed19 <soyabahmed19@gmail.com>
2023-07-04 13:23:14 +0600

    project created

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+        <header>
+          <div className="logo">
+            <img src="logo.png" alt="Logo" />
+            <h1>Company Name</h1>
+          </div>
+          <div className="search">
+            <input type="text" placeholder="Search..." />
+            <button>Search</button>
+          </div>
+        </header>
+      );
+    };
+    
+
+export default App;
